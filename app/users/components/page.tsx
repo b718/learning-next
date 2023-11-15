2fe1d8b0e3d2d32c6abebade8580339762ecf92f
Bryan Zhao <bryan718@students.cs.ubc.ca>
2023-11-14 23:44:06 -0800

    Learning Next

---
+++ app/users/components/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./page.css";
+const testingPage = () => {
+  return (
+    <div className="testing-page p-32 bg-slate-900 hover:bg-gray-800">
+      testingPage
+    </div>
+  );
+};
+
+export default testingPage;
---
+++ app/users/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestingFirstPage = () => {
+  return <div>TestingFirstPage</div>;
+};
+
+export default TestingFirstPage;
